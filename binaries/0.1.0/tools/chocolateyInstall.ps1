6275f82446cd8a81d0da44031fbc91587547a414
Craig Maslowski <cmaslowski@fairwaytech.com>
2013-03-07 11:40:17 -0800

    added license added binaries and chocolatey package setup moved 0.1.0 binary into version folder

---
+++ binaries/0.1.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+Install-ChocolateyZipPackage 'Cupboard' 'https://github.com/craigmaslowski/cupboard/blob/master/binaries/0.1.0/Cupboard.0.1.0.zip?raw=true' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
